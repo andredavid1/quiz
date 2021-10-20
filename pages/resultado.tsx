5b59538918860aedb4ce58f19c2cd0077762ef25
andredavid1 <andredavid1@yahoo.com.br>
2021-10-20 13:30:52 -0300

    feat: create component Estatistica

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router';
+import Botao from '../components/Botao';
+import Estatistica from '../components/Estatistica';
 import styles from '../styles/Resultado.module.css';
 
 export default function Resultado() {
@@ -11,9 +13,17 @@ export default function Resultado() {
   return (
     <div className={styles.resultado}>
       <h1>Resultado Final</h1>
-      <div>{total}</div>
-      <div>{certas}</div>
-      <div>{`${percentual} %`}</div>
+      <div
+        style={{
+          display: 'flex'
+        }}
+      >
+        <Estatistica texto="Perguntas" valor={total} />
+        <Estatistica texto="Certas" valor={certas} corFundo="#9cd2a4" />
+        <Estatistica texto="Percentual" valor={`${percentual}%`} corFundo="#de6a33" />
+      </div>
+
+      <Botao href="/" texto="Tentar novamente" />
     </div>
   )
 }
